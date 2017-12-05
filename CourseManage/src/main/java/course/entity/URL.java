c6915b45885e1a70b373d742610c002cf984168a
xgqiang <1426153084@qq.com>
2017-12-05 19:35:06 +0800

    CMS

---
+++ CourseManage/src/main/java/course/entity/URL.java
@@ -0,0 +1,15 @@
+package course.entity;
+
+public class URL {
+	private String url;
+	public URL() {
+		
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
